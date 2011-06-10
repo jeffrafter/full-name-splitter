21e27ab3f5a5d2d06c5517a6134b04c15eb95712
CyberSecure <cybersecure@webportal.(none)>
2011-06-10 11:36:42 +1000

    Added HONORIFICS and options to change accessor names

--- lib/full-name-splitter.rb
+++ lib/full-name-splitter.rb
@@ -3,11 +3,22 @@
 module FullNameSplitter
 
   PREFIXES = %w(de da la du del dei vda. dello della degli delle van von der den heer ten ter vande vanden vander voor ver aan mc mac ben ibn bint al).freeze
+  HONORIFICS = %w(mr mrs miss ms dr capt ofc rev prof sir cr hon).freeze
+
+  attr_accessor :full_name_splitter_options
+  def full_name_splitter_options
+    @full_name_splitter_options ||= {
+      :honorific  => :honorific,
+      :first_name => :first_name,
+      :last_name  => :last_name
+    }
+  end
 
   class Splitter
     
     def initialize(full_name)
       @full_name  = full_name
+      @honorific = []
       @first_name = []
       @last_name  = []
       split!
@@ -16,7 +27,9 @@ module FullNameSplitter
     def split!
       @units = @full_name.split(/\s+/)
       while @unit = @units.shift do
-        if prefix? or with_apostrophe? or (first_name? and last_unit? and not initial?)
+        if honorific?
+          @honorific << @unit
+        elsif prefix? or with_apostrophe? or (first_name? and last_unit? and not initial?)
           @last_name << @unit and break
         else
           @first_name << @unit
@@ -25,6 +38,10 @@ module FullNameSplitter
       @last_name += @units
 
       adjust_exceptions!
+    end
+
+    def honorific
+      @honorific.empty? ? nil : @honorific[0].gsub(/[^\w]/, '')
     end
 
     def first_name
@@ -36,6 +53,10 @@ module FullNameSplitter
     end
 
     private
+
+    def honorific?
+      HONORIFICS.include?(@unit.downcase.gsub(/[^\w]/, '')) && @honorific.empty? && @first_name.empty? && @last_name.empty?
+    end
 
     def prefix?
       PREFIXES.include?(@unit.downcase)
@@ -79,11 +100,16 @@ module FullNameSplitter
   end
   
   def full_name
-    [first_name, last_name].compact.join(' ')
+    o = full_name_splitter_options
+    ["#{__send__ o[:honorific]}.", __send__(o[:first_name]), __send__(o[:last_name])].compact.join(' ')
   end
   
   def full_name=(name)
-    self.first_name, self.last_name = split(name)
+    o = full_name_splitter_options
+    parts = split name
+    __send__ "#{o[:honorific]}=",  parts[0]
+    __send__ "#{o[:first_name]}=", parts[1]
+    __send__ "#{o[:last_name]}=",  parts[2]
   end
   
   private 
@@ -97,7 +123,7 @@ module FullNameSplitter
         map{ |u| u.empty? ? nil : u }   # but it should be [nil, "van helsing"] by lib convection
     else
       splitter = Splitter.new(name)
-      [splitter.first_name, splitter.last_name]
+      [splitter.honorific, splitter.first_name, splitter.last_name]
     end
   end
   
--- spec/lib/full-name-splitter_spec.rb
+++ spec/lib/full-name-splitter_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 class Incognito
   include FullNameSplitter
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :honorific
 end
 
 describe Incognito do
@@ -25,93 +25,103 @@ describe Incognito do
     #
 
     {
-      "John Smith"                    => ["John",           "Smith"               ],
+      "John Smith"                    => [nil, "John",           "Smith"               ],
+      "Kevin J. O'Connor"             => [nil, "Kevin J.",       "O'Connor"            ],
+      "Gabriel Van Helsing"           => [nil, "Gabriel",        "Van Helsing"         ],
+      "Pierre de Montesquiou"         => [nil, "Pierre",         "de Montesquiou"      ],
+      "Charles d'Artagnan"            => [nil, "Charles",        "d'Artagnan"          ],
+      "Jaazaniah ben Shaphan"         => [nil, "Jaazaniah",      "ben Shaphan"         ],
+      "Noda' bi-Yehudah"              => [nil, "Noda'",          "bi-Yehudah"          ],
+      "Maria del Carmen Menendez"     => [nil, "Maria",          "del Carmen Menendez" ],
+      "Alessandro Del Piero"          => [nil, "Alessandro",     "Del Piero"           ],
 
-      "Kevin J. O'Connor"             => ["Kevin J.",       "O'Connor"            ],
-      "Gabriel Van Helsing"           => ["Gabriel",        "Van Helsing"         ],
-      "Pierre de Montesquiou"         => ["Pierre",         "de Montesquiou"      ],
-      "Charles d'Artagnan"            => ["Charles",        "d'Artagnan"          ],
-      "Jaazaniah ben Shaphan"         => ["Jaazaniah",      "ben Shaphan"         ],
-      "Noda' bi-Yehudah"              => ["Noda'",          "bi-Yehudah"          ],
-      "Maria del Carmen Menendez"     => ["Maria",          "del Carmen Menendez" ],
-      "Alessandro Del Piero"          => ["Alessandro",     "Del Piero"           ],
+      "George W Bush"                 => [nil, "George W",       "Bush"                ],
+      "George H. W. Bush"             => [nil, "George H. W.",   "Bush"                ],
+      "James K. Polk"                 => [nil, "James K.",       "Polk"                ],
+      "William Henry Harrison"        => [nil, "William Henry",  "Harrison"            ],
+      "John Quincy Adams"             => [nil, "John Quincy",    "Adams"               ],
 
-      "George W Bush"                 => ["George W",       "Bush"                ],
-      "George H. W. Bush"             => ["George H. W.",   "Bush"                ],
-      "James K. Polk"                 => ["James K.",       "Polk"                ],
-      "William Henry Harrison"        => ["William Henry",  "Harrison"            ],
-      "John Quincy Adams"             => ["John Quincy",    "Adams"               ],
+      "John Quincy"                   => [nil, "John",           "Quincy"              ],
+      "George H. W."                  => [nil, "George H. W.",   nil                   ],
+      "Van Helsing"                   => [nil, nil,              "Van Helsing"         ],
+      "d'Artagnan"                    => [nil, nil,              "d'Artagnan"          ],
+      "O'Connor"                      => [nil, nil,              "O'Connor"            ],
 
-      "John Quincy"                   => ["John",           "Quincy"              ],
-      "George H. W."                  => ["George H. W.",   nil                   ],
-      "Van Helsing"                   => [nil,              "Van Helsing"         ],
-      "d'Artagnan"                    => [nil,              "d'Artagnan"          ],
-      "O'Connor"                      => [nil,              "O'Connor"            ],
+      "George"                        => [nil, "George",         nil                   ],
+      "Kevin J. "                     => [nil, "Kevin J.",       nil                   ],
 
-      "George"                        => ["George",         nil                   ],
-      "Kevin J. "                     => ["Kevin J.",       nil                   ],
-
-      "Thomas G. Della Fave"          => ["Thomas G.",      "Della Fave"          ],
-      "Anne du Bourg"                 => ["Anne",           "du Bourg"            ],
+      "Thomas G. Della Fave"          => [nil, "Thomas G.",      "Della Fave"          ],
+      "Anne du Bourg"                 => [nil, "Anne",           "du Bourg"            ],
 
       # German
-      "Johann Wolfgang von Goethe"    => ["Johann Wolfgang", "von Goethe"         ],
+      "Johann Wolfgang von Goethe"    => [nil, "Johann Wolfgang", "von Goethe"         ],
 
       # Spanish-speaking countries
-      "Juan Martín de la Cruz Gómez"  => ["Juan Martín",    "de la Cruz Gómez"    ],
-      "Javier Reyes de la Barrera"    => ["Javier",         "Reyes de la Barrera" ],
+      "Juan Martín de la Cruz Gómez"  => ["Jnil, uan Martín",    "de la Cruz Gómez"    ],
+      "Javier Reyes de la Barrera"    => [nil, "Javier",         "Reyes de la Barrera" ],
       "Rosa María Pérez Martínez Vda. de la Cruz" =>
-                                         ["Rosa María",    "Pérez Martínez Vda. de la Cruz"],
+                                         [nil, "Rosa María",    "Pérez Martínez Vda. de la Cruz"],
 
       # Italian
-      "Federica Pellegrini"           => ["Federica",       "Pellegrini"          ],
-      "Leonardo da Vinci"             => ["Leonardo",       "da Vinci"            ],
+      "Federica Pellegrini"           => [nil, "Federica",       "Pellegrini"          ],
+      "Leonardo da Vinci"             => [nil, "Leonardo",       "da Vinci"            ],
       # sounds like a fancy medival action movie star pseudonim
-      "Alberto Del Sole"              => ["Alberto",        "Del Sole"            ],
+      "Alberto Del Sole"              => [nil, "Alberto",        "Del Sole"            ],
       # horror movie star pseudonim?
-      "Adriano Dello Spavento"        => ["Adriano",        "Dello Spavento"      ],
-      "Luca Delle Fave"               => ["Luca",           "Delle Fave"          ],
-      "Francesca Della Valle"         => ["Francesca",      "Della Valle"         ],
-      "Guido Delle Colonne"           => ["Guido",          "Delle Colonne"       ],
-      "Tomasso D'Arco"                => ["Tomasso",        "D'Arco"              ],
+      "Adriano Dello Spavento"        => [nil, "Adriano",        "Dello Spavento"      ],
+      "Luca Delle Fave"               => [nil, "Luca",           "Delle Fave"          ],
+      "Francesca Della Valle"         => [nil, "Francesca",      "Della Valle"         ],
+      "Guido Delle Colonne"           => [nil, "Guido",          "Delle Colonne"       ],
+      "Tomasso D'Arco"                => [nil, "Tomasso",        "D'Arco"              ],
 
       # Dutch
-      "Johan de heer Van Kampen"      => ["Johan",          "de heer Van Kampen"  ],
-      "Han Van De Casteele"           => ["Han",            "Van De Casteele"     ],
-      "Han Vande Casteele"            => ["Han",            "Vande Casteele"      ],
+      "Johan de heer Van Kampen"      => [nil, "Johan",          "de heer Van Kampen"  ],
+      "Han Van De Casteele"           => [nil, "Han",            "Van De Casteele"     ],
+      "Han Vande Casteele"            => [nil, "Han",            "Vande Casteele"      ],
 
       # Arabic
-      "Suraih ibn Hani"               => ["Suraih",         "ibn Hani"            ],
-      "Sumayya bint Khubbat"          => ["Sumayya",        "bint Khubbat"        ],
-      "Muhammad al-Hallaj"            => ["Muhammad",       "al-Hallaj"           ],
-      "Layla bint Zuhayr ibn Yazid al-Nahdiyah" => ["Layla","bint Zuhayr ibn Yazid al-Nahdiyah"],
+      "Suraih ibn Hani"               => [nil, "Suraih",         "ibn Hani"            ],
+      "Sumayya bint Khubbat"          => [nil, "Sumayya",        "bint Khubbat"        ],
+      "Muhammad al-Hallaj"            => [nil, "Muhammad",       "al-Hallaj"           ],
+      "Layla bint Zuhayr ibn Yazid al-Nahdiyah" => [nil, "Layla","bint Zuhayr ibn Yazid al-Nahdiyah"],
 
       # Exceptions?
       # the architect Ludwig Mies van der Rohe, from the West German city of Aachen, was originally Ludwig Mies;
-      "Ludwig Mies van der Rohe"      => ["Ludwig",         "Mies van der Rohe"   ],
+      "Ludwig Mies van der Rohe"      => [nil, "Ludwig",         "Mies van der Rohe"   ],
 
       # If comma is provided then split by comma
 
-      "John, Quincy Adams"             => ["John",    "Quincy Adams"              ],
-      "Ludwig Mies, van der Rohe"      => ["Ludwig Mies", "van der Rohe"          ],
+      "John, Quincy Adams"             => [nil, "John",    "Quincy Adams"              ],
+      "Ludwig Mies, van der Rohe"      => [nil, "Ludwig Mies", "van der Rohe"          ],
 
       # Test ignoring unnecessary whitespaces
-      "\t Ludwig  Mies\t van der Rohe "   => ["Ludwig", "Mies van der Rohe"       ],
-      "\t Ludwig  Mies,\t van  der Rohe " => ["Ludwig Mies", "van der Rohe"       ],
-      "\t Ludwig      "                   => ["Ludwig", nil                       ],
-      "  van  helsing "                   => [nil, "van helsing"                  ],
-      " , van  helsing "                  => [nil, "van helsing"                  ],
-      "\t Ludwig  Mies,\t van  der Rohe " => ["Ludwig Mies", "van der Rohe"       ],
+      "\t Ludwig  Mies\t van der Rohe "   => [nil, "Ludwig", "Mies van der Rohe"       ],
+      "\t Ludwig  Mies,\t van  der Rohe " => [nil, "Ludwig Mies", "van der Rohe"       ],
+      "\t Ludwig      "                   => [nil, "Ludwig", nil                       ],
+      "  van  helsing "                   => [nil, nil, "van helsing"                  ],
+      " , van  helsing "                  => [nil, nil, "van helsing"                  ],
+      "\t Ludwig  Mies,\t van  der Rohe " => [nil, "Ludwig Mies", "van der Rohe"       ],
+
+      # Honorifics
+      "Mr John Smith"                     => ["Mr",   "John",           "Smith"        ],
+      "Miss Anne du Bourg"                => ["Miss", "Anne",           "du Bourg"     ],
+      "Ms Agatha Christie"                => ["Ms",   "Agatha",         "Christie"     ],
+      "Sir Arthur Conan Doyle"            => ["Sir",  "Arthur Conan",   "Doyle"        ],
+      "Mrs. J.K. Rowling"                 => ["Mrs",  "J.K.",           "Rowling"      ],
+      "Dr. Seuss"                         => ["Dr",   nil,              "Seuss"        ],
+      "Mr. Kevin J. O'Connor"             => ["Mr",   "Kevin J.",       "O'Connor"     ],
+      "Hon. George W Bush"                => ["Hon",  "George W",       "Bush"         ],
+      "Dr. John, Quincy Adams"            => ["Dr",   "John",    "Quincy Adams"        ],
 
     }.
 
     each do |full_name, split_name|
-      it "should split #{full_name} to '#{split_name.first}' and '#{split_name.last}'" do
+      it "should split #{full_name} to '#{split_name[0]}' and '#{split_name[1]}' and '#{split_name[2]}'" do
         subject.full_name = full_name
-        gum(subject.first_name, subject.last_name).should == gum(*split_name)
+        [subject.honorific, subject.first_name, subject.last_name].should == split_name
       end
 
-      it "should split #{full_name} to '#{split_name.first}' and '#{split_name.last}' when called as module function" do
+      it "should split #{full_name} to '#{split_name[0]}' and '#{split_name[1]}' and '#{split_name[2]}' when called as module function" do
         FullNameSplitter.split(full_name).should == split_name
       end
 
